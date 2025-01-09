5e859cad2a950ef260cf3d9072f242897a4afe48
Parthiv Gandhi <parthiv1499@gmail.com>
2025-01-08 23:14:50 -0500

    health hospital update

--- src/App.js
+++ src/App.js
@@ -238,7 +238,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Health Hospital
         </p>
         <a
           className="App-link"
